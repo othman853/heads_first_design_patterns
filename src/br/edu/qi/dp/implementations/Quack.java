79e4f011cdf75f3d2b237f0dc47226e4a870e6a5
othman853 <anuar.yasser@gmail.com>
2015-03-11 21:39:25 -0300

    #CH_01: <STRATEGY> - Implementation of QuackBehaviour

---
+++ src/br/edu/qi/dp/implementations/Quack.java
@@ -0,0 +1,10 @@
+package br.edu.qi.dp.implementations;
+
+import br.edu.qi.dp.interfaces.QuackingBehaviour;
+
+public class Quack implements QuackingBehaviour{
+	
+	public void quack(){
+		System.out.println("Quack");
+	}
+}
